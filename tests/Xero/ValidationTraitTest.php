7d2d1390f4b51ad435f8c2450ceb4df81351ff28
Fergus <strfer001@myuct.ac.za>
2017-07-27 11:53:23 +0200

    Validation on 100% coverage

--- tests/Xero/ValidationTraitTest.php
+++ tests/Xero/ValidationTraitTest.php
@@ -17,24 +17,20 @@ class ValidationTraitTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals(false, $this->isValidPrimitive(5, 'string'));
     }
 
-    public function testValidRegex(){
-        $this->validateRegex('2', '~[0-9]~');
-        $this->assertTrue(true);
-    }
-
     public function testBadRegexException()
     {
+        $this->assertEquals(null, $this->validateRegex('2', '~[0-9]~'));
         $this->expectException(ValidationException::class);
         $this->expectExceptionMessage(
             sprintf('value fergus failed to validate')
         );
         $this->expectExceptionCode(ValidationException::STRING_REGEX_MISMATCH);
-
         $this->validateRegex('fergus', '~[0-9]~');
     }
 
     public function testValidateIntegerException()
     {
+        $this->assertEquals(null, $this->validateRange(10, 5, 15));
         $this->expectException(ValidationException::class);
         $this->expectExceptionMessage(
             sprintf('Validation error value 20 out of min(5) max(15) Integer value is out of range')
@@ -45,6 +41,7 @@ class ValidationTraitTest extends \PHPUnit_Framework_TestCase
 
     public function testValidateStringException()
     {
+        $this->assertEquals(null, $this->validateRange('asd', 1, 6));
         $this->expectException(ValidationException::class);
         $this->expectExceptionMessage(
             sprintf('Validation error value AAaaaaaa out of min(5) max(6) String length is out of range')
