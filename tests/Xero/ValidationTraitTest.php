e8f721419f4ef6dd71822e606f4ea3732d0a722e
Fergus <strfer001@myuct.ac.za>
2017-07-27 12:28:58 +0200

    Started adding robustness checks for validator

--- tests/Xero/ValidationTraitTest.php
+++ tests/Xero/ValidationTraitTest.php
@@ -59,4 +59,59 @@ class ValidationTraitTest extends \PHPUnit_Framework_TestCase
         $this->expectExceptionCode(ValidationException::VALIDATION_TYPE_ERROR);
         $this->validateRange(7.2336, 5, 15);
     }
+
+    //above tests reach 100% coverage, robustness tests below
+
+    public function testValidateNull()
+    {
+        $this->expectException(ValidationException::class);
+        $this->expectExceptionMessage(
+            sprintf('Validation error value  is type NULL Validation type is invalid')
+        );
+        $this->expectExceptionCode(ValidationException::VALIDATION_TYPE_ERROR);
+        $this->validateRange(null, 5, 15);
+    }
+
+    public function testValidateNullCharacter()
+    {
+        //These strings need the double inverted comma for the null character to register properly
+        $this->expectException(ValidationException::class);
+        $this->expectExceptionMessage(
+            sprintf("Validation error value \0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0asdhd out of min(5) max(15) String length is out of range")
+        );
+        $this->expectExceptionCode(ValidationException::STRING_LENGTH_OUT_OF_RANGE);
+        $this->validateRange("\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0asdhd", 5, 15);
+    }
+
+    public function testValidateNullType()
+    {
+        //Should this be valid?
+        $this->assertTrue($this->isValidPrimitive("\0foo", 'string'));
+
+    }
+
+    public function testValidateDifferentNullType()
+    {
+        //Should this be valid?
+        $this->assertTrue($this->isValidPrimitive("\x00foo", 'string'));
+
+    }
+
+    public function testValidateEncodedType()
+    {
+        //URLEncoding
+        //Should this be valid?
+        $this->assertTrue($this->isValidPrimitive("foo%60%7ebar", 'string'));
+        //Should this be valid?
+        $this->assertTrue($this->isValidPrimitive(urldecode("foo%60%7ebar"), 'string'));
+
+        //HexEncoding
+        $this->assertTrue($this->isValidPrimitive(bin2hex("foobar"), 'string'));
+        $this->expectException(ValidationException::class);
+        $this->expectExceptionMessage(
+            sprintf("Validation error value 666f6f0a626172 out of min(5) max(10) String length is out of range")
+        );
+        $this->expectExceptionCode(ValidationException::STRING_LENGTH_OUT_OF_RANGE);
+        $this->validateRange(bin2hex("foo\nbar"), 5, 10);
+    }
 }
