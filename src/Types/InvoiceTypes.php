e71921985fe9ec58618739ad9cc595387e29ff94
Igor <igorsergiichuk@gmail.com>
2017-08-04 16:11:17 +0300

    InvoiceTypes + tests

--- src/Types/InvoiceTypes.php
+++ src/Types/InvoiceTypes.php
@@ -1,7 +1,23 @@
 <?php
+
+namespace DarrynTen\Xero\Types;
+
 /**
- * Created by PhpStorm.
- * User: igor
- * Date: 04.08.17
- * Time: 16:06
+ * Xero Invoice Types
+ *
+ * @category Types
+ * @package  XeroPHP
+ * @author   Igor Sergiichuk <igorsergiichuk@github.com>
+ * @license  MIT <https://github.com/darrynten/xero-accounting-php/LICENSE>
+ * @link     https://github.com/darrynten/xero-accounting-php
  */
+class InvoiceTypes
+{
+    const ACCPAY = 'ACCPAY';
+    const ACCREC = 'ACCREC';
+
+    public $descriptions = [
+        self::ACCPAY => 'A bill - commonly known as a Accounts Payable or supplier invoice',
+        self::ACCREC => 'A sales invoice - commonly known as an Accounts Receivable or customer invoice',
+    ];
+}
--- tests/Xero/Types/InvoiceTypesTest.php
+++ tests/Xero/Types/InvoiceTypesTest.php
@@ -1,7 +1,25 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: igor
- * Date: 04.08.17
- * Time: 16:08
- */
+
+namespace DarrynTen\Xero\Tests\Xero\Types;
+
+use DarrynTen\Xero\Types\InvoiceTypes;
+
+class InvoiceTypesTest extends \PHPUnit_Framework_TestCase
+{
+    public function testInvoiceTypesConstants()
+    {
+        $this->assertEquals(InvoiceTypes::ACCPAY, 'ACCPAY');
+        $this->assertEquals(InvoiceTypes::ACCREC, 'ACCREC');
+    }
+
+    public function testInvoiceTypesDescriptions()
+    {
+        $invoiceTypes = new InvoiceTypes;
+        $expected = [
+            InvoiceTypes::ACCPAY => 'A bill - commonly known as a Accounts Payable or supplier invoice',
+            InvoiceTypes::ACCREC => 'A sales invoice - commonly known as an Accounts Receivable or customer invoice',
+        ];
+
+        $this->assertEquals($expected, $invoiceTypes->descriptions);
+    }
+}
