8c806ddbb224888b6e4f25f7baf9e12ea46f5911
Igor <igorsergiichuk@gmail.com>
2017-08-04 14:38:30 +0300

    CreditNoteTypes + tests

--- src/Types/CreditNoteTypes.php
+++ src/Types/CreditNoteTypes.php
@@ -1,7 +1,23 @@
 <?php
+
+namespace DarrynTen\Xero\Types;
+
 /**
- * Created by PhpStorm.
- * User: igor
- * Date: 04.08.17
- * Time: 14:25
+ * Xero Credit Note Types
+ *
+ * @category Types
+ * @package  XeroPHP
+ * @author   Igor Sergiichuk <igorsergiichuk@github.com>
+ * @license  MIT <https://github.com/darrynten/xero-accounting-php/LICENSE>
+ * @link     https://github.com/darrynten/xero-accounting-php
  */
+class CreditNoteTypes
+{
+    const ACCPAYCREDIT = 'ACCPAYCREDIT';
+    const ACCRECCREDIT = 'ACCRECCREDIT';
+
+    public $descriptions = [
+        self::ACCPAYCREDIT => 'An Accounts Payable(supplier) Credit Note',
+        self::ACCRECCREDIT => 'An Account Receivable(customer) Credit Note',
+    ];
+}
--- tests/Xero/Types/CreditNoteTypesTest.php
+++ tests/Xero/Types/CreditNoteTypesTest.php
@@ -1,7 +1,25 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: igor
- * Date: 04.08.17
- * Time: 14:30
- */
+
+namespace DarrynTen\Xero\Tests\Xero\Types;
+
+use DarrynTen\Xero\Types\CreditNoteTypes;
+
+class CreditNoteTypesTest extends \PHPUnit_Framework_TestCase
+{
+    public function testCreditNoteTypesConstants()
+    {
+        $this->assertEquals(CreditNoteTypes::ACCPAYCREDIT, 'ACCPAYCREDIT');
+        $this->assertEquals(CreditNoteTypes::ACCRECCREDIT, 'ACCRECCREDIT');
+    }
+
+    public function testCreditNoteTypesDescriptions()
+    {
+        $creditNoteTypes = new CreditNoteTypes;
+        $expected = [
+            CreditNoteTypes::ACCPAYCREDIT => 'An Accounts Payable(supplier) Credit Note',
+            CreditNoteTypes::ACCRECCREDIT => 'An Account Receivable(customer) Credit Note',
+        ];
+
+        $this->assertEquals($expected, $creditNoteTypes->descriptions);
+    }
+}
