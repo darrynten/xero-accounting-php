e7729c3125d59ddaf7e89742fe6f375300d84c66
Igor <igorsergiichuk@gmail.com>
2017-08-04 16:54:01 +0300

    PaymentTypes + tests

--- src/Types/PaymentTypes.php
+++ src/Types/PaymentTypes.php
@@ -1,7 +1,35 @@
 <?php
+
+namespace DarrynTen\Xero\Types;
+
 /**
- * Created by PhpStorm.
- * User: igor
- * Date: 04.08.17
- * Time: 16:31
+ * Xero Payment Types
+ *
+ * @category Types
+ * @package  XeroPHP
+ * @author   Igor Sergiichuk <igorsergiichuk@github.com>
+ * @license  MIT <https://github.com/darrynten/xero-accounting-php/LICENSE>
+ * @link     https://github.com/darrynten/xero-accounting-php
  */
+class PaymentTypes
+{
+    const ACCRECPAYMENT = 'ACCRECPAYMENT';
+    const ACCPAYPAYMENT = 'ACCPAYPAYMENT';
+    const ARCREDITPAYMENT = 'ARCREDITPAYMENT';
+    const APCREDITPAYMENT = 'APCREDITPAYMENT';
+    const APOVERPAYMENTPAYMENT = 'APOVERPAYMENTPAYMENT';
+    const AROVERPAYMENTPAYMENT = 'AROVERPAYMENTPAYMENT';
+    const ARPREPAYMENTPAYMENT = 'ARPREPAYMENTPAYMENT';
+    const APPREPAYMENTPAYMENT = 'APPREPAYMENTPAYMENT';
+
+    public $descriptions = [
+        self::ACCRECPAYMENT => 'Accounts Receivable Payment',
+        self::ACCPAYPAYMENT => 'Accounts Payable Payment',
+        self::ARCREDITPAYMENT => 'Accounts Receivable Credit Payment (Refund)',
+        self::APCREDITPAYMENT => 'Accounts Payable Credit Payment (Refund)',
+        self::AROVERPAYMENTPAYMENT => 'Accounts Receivable Overpayment Payment (Refund)',
+        self::ARPREPAYMENTPAYMENT => 'Accounts Receivable Prepayment Payment (Refund)',
+        self::APPREPAYMENTPAYMENT => 'Accounts Payable Prepayment Payment (Refund)',
+        self::APOVERPAYMENTPAYMENT => 'Accounts Payable Overpayment Payment (Refund)',
+    ];
+}
--- tests/Xero/Types/PaymentTypesTest.php
+++ tests/Xero/Types/PaymentTypesTest.php
@@ -1,7 +1,37 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: igor
- * Date: 04.08.17
- * Time: 16:39
- */
+
+namespace DarrynTen\Xero\Tests\Xero\Types;
+
+use DarrynTen\Xero\Types\PaymentTypes;
+
+class PaymentTypesTest extends \PHPUnit_Framework_TestCase
+{
+    public function testPaymentTypesConstants()
+    {
+        $this->assertEquals(PaymentTypes::ACCRECPAYMENT, 'ACCRECPAYMENT');
+        $this->assertEquals(PaymentTypes::ACCPAYPAYMENT, 'ACCPAYPAYMENT');
+        $this->assertEquals(PaymentTypes::ARCREDITPAYMENT, 'ARCREDITPAYMENT');
+        $this->assertEquals(PaymentTypes::APCREDITPAYMENT, 'APCREDITPAYMENT');
+        $this->assertEquals(PaymentTypes::ARPREPAYMENTPAYMENT, 'ARPREPAYMENTPAYMENT');
+        $this->assertEquals(PaymentTypes::APPREPAYMENTPAYMENT, 'APPREPAYMENTPAYMENT');
+        $this->assertEquals(PaymentTypes::APOVERPAYMENTPAYMENT, 'APOVERPAYMENTPAYMENT');
+        $this->assertEquals(PaymentTypes::AROVERPAYMENTPAYMENT, 'AROVERPAYMENTPAYMENT');
+    }
+
+    public function testPaymentTypesDescriptions()
+    {
+        $paymentTypes = new PaymentTypes;
+        $expected = [
+            PaymentTypes::ACCRECPAYMENT => 'Accounts Receivable Payment',
+            PaymentTypes::ACCPAYPAYMENT => 'Accounts Payable Payment',
+            PaymentTypes::ARCREDITPAYMENT => 'Accounts Receivable Credit Payment (Refund)',
+            PaymentTypes::APCREDITPAYMENT => 'Accounts Payable Credit Payment (Refund)',
+            PaymentTypes::ARPREPAYMENTPAYMENT => 'Accounts Receivable Prepayment Payment (Refund)',
+            PaymentTypes::APPREPAYMENTPAYMENT => 'Accounts Payable Prepayment Payment (Refund)',
+            PaymentTypes::APOVERPAYMENTPAYMENT => 'Accounts Payable Overpayment Payment (Refund)',
+            PaymentTypes::AROVERPAYMENTPAYMENT => 'Accounts Receivable Overpayment Payment (Refund)',
+        ];
+
+        $this->assertEquals($expected, $paymentTypes->descriptions);
+    }
+}
